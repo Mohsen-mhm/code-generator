c287f1f3ae4a6e8a347ebfded491bda27a500929
MOHSEN <mohsen.mhm23@gmail.com>
2025-03-21 15:21:26 +0330

    Hotfix

--- src/Generators/BaseGenerator.php
+++ src/Generators/BaseGenerator.php
@@ -186,16 +186,16 @@ abstract class BaseGenerator
      */
     protected function getStubContents($stub, $replacements = [])
     {
-        $stubPath = __DIR__ . '/../../stubs/' . $stub . '.stub';
+        $stubPath = __DIR__ . '/../../Stubs/' . $stub . '.stub';
         
         // Check if the stub exists in the package
         if (!file_exists($stubPath)) {
             // Try to find the stub in the vendor directory
-            $stubPath = base_path('vendor/mohsen-mhm/code-generator/stubs/' . $stub . '.stub');
+            $stubPath = base_path('vendor/mohsen-mhm/code-generator/Stubs/' . $stub . '.stub');
             
             // If still not found, try the published stubs
             if (!file_exists($stubPath)) {
-                $stubPath = base_path('stubs/code-generator/' . $stub . '.stub');
+                $stubPath = base_path('Stubs/code-generator/' . $stub . '.stub');
             }
         }
         
